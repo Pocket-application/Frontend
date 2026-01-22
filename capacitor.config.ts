071170d541bf5249203121a887a846efefb988be
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2026-01-21 20:11:26 -0500

    app android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.oscarpalomino.pocket',
+  appName: 'pocket',
+  webDir: 'dist'
+};
+
+export default config;
